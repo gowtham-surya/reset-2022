8099f17d6892f03bd11a872e2bb23bbfa999691e
gowtham-surya <gowthamsurya95855@gmail.com>
2022-12-01 20:00:31 +0530

    add scroll spy and corrections

--- js/script.js
+++ js/script.js
@@ -9,3 +9,25 @@ $(document).ready(() => {
     $('.menu-items').removeClass('active');
   });
 });
+
+// scroll spy
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('.menu-items ul li a');
+
+window.onscroll = () => {
+  section.forEach((sec) => {
+    var top = window.scrollY;
+    var id = sec.getAttribute('id');
+    var height = sec.offsetHeight;
+    var offset = sec.offsetTop - 20;
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove('current');
+        document
+          .querySelector('.menu-items ul li a[href*=' + id + ']')
+          .classList.add('current');
+      });
+    }
+  });
+};
