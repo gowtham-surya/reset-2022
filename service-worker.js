987576752a783a4af3b1aa5f68b8ee48f435708d
GOWDHAM P <103630404+gowtham-surya@users.noreply.github.com>
2022-12-02 10:15:54 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,15 @@
 //service worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open("cache-files-v1")
+      .then((cache) =>
+        cache.addAll([
+          "/index.html",
+          "/css/style.css",
+          "/js/script.js"
+        ])
+      )
+  );
+});
+
